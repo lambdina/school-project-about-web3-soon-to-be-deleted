ed1f1ef44172bac3baf3e220bc496e704b730944
Adina <adina.cazalens@epitech.eu>
2024-04-13 04:42:34 +0200

    2e tentative cors

--- realestate/settings.py
+++ realestate/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = [
     '*'
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://web3-git-main-lybertyxzs-projects.vercel.app",
+]
 
 # Application definition
 
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_swagger',
     'rest_framework.authtoken',
